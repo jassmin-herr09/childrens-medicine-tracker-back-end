7a59a37dda8a98d886f6005e059bc18ec450d864
jassmin-herr <jherr0905@gmail.com>
2021-06-08 14:39:44 -0500

    wip: connected to mongoDb

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
+//var cors = require('cors');
+var medicinesRouter = require('./routes/medicines');
 
 
 var app = express();
 
+require('dotenv').config();
+require('./config/database');
+
 app.use(logger('dev'));
 app.use(express.json());
+//app.use(cors());
 
 
-app.use('/', indexRouter);
+app.use('/api/medicines', medicinesRouter);
 
 
 module.exports = app;
